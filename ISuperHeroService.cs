c39a6133ed027705e7dff877de0e8f9a07687ca5
Mohammad Afsar <140788344+Mafsar1@users.noreply.github.com>
2023-09-26 12:03:24 +0530

    Add files via upload

---
+++ ISuperHeroService.cs
@@ -0,0 +1,14 @@
+﻿using SuperHeroAPIDotNet7.Models;
+
+namespace SuperHeroAPIDotNet7.Repository.IServices
+{
+    public interface ISuperHeroService
+    {
+        Task<List<SuperHero>> GetAllHeroes();
+        Task<SuperHero?> GetSingleHero(int id);
+        Task<List<SuperHero>> AddHero(SuperHero hero);
+        Task<List<SuperHero>?> UpdateHero(int id, SuperHero request);
+        Task<List<SuperHero>?> DeleteHero(int id);
+        
+    }
+}
---
+++ SuperHeroService.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperHeroAPIDotNet7.Data;
+using SuperHeroAPIDotNet7.Models;
+using SuperHeroAPIDotNet7.Repository.IServices;
+
+namespace SuperHeroAPIDotNet7.Repository.Services
+{
+    public class SuperHeroService: ISuperHeroService
+    {
+        private readonly DataContext _context;
+
+        public SuperHeroService(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<SuperHero>> AddHero(SuperHero hero)
+        {
+            _context.SuperHeroes.Add(hero);
+            await _context.SaveChangesAsync();
+            return await _context.SuperHeroes.ToListAsync();
+        }
+        
+        public async Task<List<SuperHero>?> DeleteHero(int id)
+        {
+            var hero = await _context.SuperHeroes.FindAsync(id);
+            if (hero == null)
+                return null;
+
+            _context.SuperHeroes.Remove(hero);
+            await _context.SaveChangesAsync();
+
+            return await _context.SuperHeroes.ToListAsync();
+        }
+
+        public async Task<List<SuperHero>> GetAllHeroes()
+        {
+            var heroes = await _context.SuperHeroes.ToListAsync();
+            return heroes;
+        }
+
+        public async Task<SuperHero?> GetSingleHero(int id)
+        {
+            var hero = await _context.SuperHeroes.FindAsync(id);
+            if (hero == null)
+                return null;
+
+            return hero;
+        }
+
+        public async Task<List<SuperHero>?> UpdateHero(int id, SuperHero request)
+        {
+            var hero = await _context.SuperHeroes.Where(x=> x.Id == id).FirstOrDefaultAsync();
+            if (hero == null)
+                return null;
+
+            hero.FirstName = request.FirstName;
+            hero.LastName = request.LastName;
+            hero.Name = request.Name;
+            hero.Place = request.Place;
+            hero.Description = request.Description;
+
+            await _context.SaveChangesAsync();
+
+            return await _context.SuperHeroes.ToListAsync();
+        }
+    }
+
+}
